15914090101ae8bb117861af340e4039d30bd076
Joel Gustavo Carhuarica Aguilar <joel.carhuarica.a@uni.pe>
2026-04-26 00:44:55 -0500

    test: configuramos  estructura base de pruebas unitarias y cabeceras

---
+++ tests/test_main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cassert>     // Para usar assert()
+#include <algorithm>   // Para usar std::find
+#include <vector>
+#include "../include/Solution.h"
+
+// En este commit solo dejare las firmas de lo que probare después
+void test_caso_basico();
+void test_sin_coincidencias();
+void test_palabra_larga();
+
+int main() {
+    std::cout << "--- Iniciando Pruebas Unitarias ---" << std::endl;
+    
+    // Las llamadas a las funciones se comentan o se dejan para el siguiente commit
+    // test_caso_basico();
+    
+    std::cout << "--- Pruebas finalizadas ---" << std::endl;
+    return 0;
+}
