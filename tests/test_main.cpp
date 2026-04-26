7a7e8f9ddcbc2b3bf40a55c0f1a8fe1045c736c0
Joel Gustavo Carhuarica Aguilar <joel.carhuarica.a@uni.pe>
2026-04-26 01:48:33 -0500

    test:implementar_caso_de_prueba_básico_para_validación_de_resultados_positivos"

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -4,16 +4,28 @@
 #include <vector>
 #include "../include/Solution.h"
 
-// En este commit solo dejare las firmas de lo que probare después
-void test_caso_basico();
+void test_caso_basico() {
+    Solution sol;
+    std::vector<std::vector<char>> board = {{'a','b'},{'c','d'}};
+    std::vector<std::string> words = {"ab", "ac", "abcd", "dcba"};
+    
+    std::vector<std::string> res = sol.findWords(board, words);
+    
+    // Verificamos que encuentre exactamente 2 palabras ("ab" y "ac")
+    // "abcd" no se puede porque 'd' no está al lado de 'b' o 'c' en este tablero
+    assert(res.size() == 2);
+    assert(std::find(res.begin(), res.end(), "ab") != res.end());
+    assert(std::find(res.begin(), res.end(), "ac") != res.end());
+    std::cout << "[PASSED] Test Caso Basico" << std::endl;
+}
+
 void test_sin_coincidencias();
 void test_palabra_larga();
 
 int main() {
     std::cout << "--- Iniciando Pruebas Unitarias ---" << std::endl;
     
-    // Las llamadas a las funciones se comentan o se dejan para el siguiente commit que hare
-    // test_caso_basico();
+    test_caso_basico(); // <--- Ahora sí la llamamos
     
     std::cout << "--- Pruebas finalizadas ---" << std::endl;
     return 0;
