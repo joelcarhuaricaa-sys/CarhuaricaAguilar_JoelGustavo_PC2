a9780f7e5b2ab40bae6942638ab2548900571da5
Joel Gustavo Carhuarica Aguilar <joel.carhuarica.a@uni.pe>
2026-04-27 01:58:13 -0500

    refactor: completamos la definición de la clase Solution y prototipos de métodos para habilitar la compilación

--- include/Solution.h
+++ include/Solution.h
@@ -1,3 +1,7 @@
+#ifndef SOLUTION_H
+#define SOLUTION_H
+
+#include <vector>
 #include <string>
 #include <list>
 
@@ -6,3 +10,15 @@ struct TrieNode {
     bool isWord;
     TrieNode();
 };
+
+class Solution {
+public:
+    std::vector<std::string> findWords(std::vector<std::vector<char>>& board, std::vector<std::string>& words);
+
+private:
+    void backtracking(int r, int c, TrieNode* node, std::string word, 
+                      std::vector<std::vector<char>>& board, std::list<std::string>& foundList, 
+                      int ROWS, int COLS);
+};
+
+#endif
